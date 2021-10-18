bd5d8a89b4db28aa53062df86db1940e3a467d2c
Dave <dave.cadelina@outlook.com>
2021-10-18 22:20:15 +1100

    got google maps up and running

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User'
 import { Company } from './Company'
 
@@ -7,3 +8,10 @@ console.log(user)
 const company = new Company();
 console.log(company)
 
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  }
+});
