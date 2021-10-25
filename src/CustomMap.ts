ce86aa413b32a182b06b6b7cf9fc18a799ad33d9
Dave <dave.cadelina@outlook.com>
2021-10-26 03:25:03 +1100

    started creating marker functionality

---
+++ src/CustomMap.ts
@@ -0,0 +1,26 @@
+import { User } from './User'
+import { Company } from './Company'
+
+
+export class CustomMap {
+    private googleMap: google.maps.Map;
+
+    constructor(divID: string) {
+        this.googleMap = new google.maps.Map(document.getElementById(`${divID}`), {
+            zoom: 1,
+            center: {
+              lat: 0,
+              lng: 0,
+            }
+          });
+    }
+
+
+    addUserMarker(user: User): void {
+      
+    }
+
+    addCompanyMarker(company: Company): void {
+
+    }
+}
